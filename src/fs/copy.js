3f7a7e9bf590b6a022645f9cde72926035a911a6
Toustsik Mikahil <toustsikmikhail@gmail.com>
2023-06-16 16:22:28 +0200

    faet: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from "fs";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+path
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const sourceDir = path.join(__dirname, 'files');
+const destinationDir = path.join(__dirname, 'files_copy')
+
 const copy = async () => {
-    // Write your code here 
+
+  if (fs.existsSync(destinationDir)) {
+    throw new Error('FS operation failed')
+  }
+  fs.mkdir(destinationDir, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    fs.cp(sourceDir, destinationDir, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log('Directory created successfully!');
+    })
+  });
 };
 
 await copy();
