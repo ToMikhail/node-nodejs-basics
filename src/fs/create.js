b4911b63820699b937afe3de95269ac453533123
Toustsik Mikahil <toustsikmikhail@gmail.com>
2023-06-16 14:17:53 +0200

    feat: add create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+// const fs = require('fs');
+// var path = require("path");
+import fs from "fs";
+import { fileURLToPath } from 'url';
+import path from "path";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const directory = path.join(__dirname, 'files', 'fresh.txt')
+
 const create = async () => {
-    // Write your code here 
+  try {
+    if (fs.existsSync(directory)) {
+      throw new Error('FS operation failed');
+    }
+    fs.writeFile(directory, 'I am fresh and young', function (err) {
+      if (err) console.error(err);
+      console.log('File is saved');
+    })
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await create();
