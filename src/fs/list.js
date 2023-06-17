5efc2d0772b622f61ce8732efe6722a47eb508de
Toustsik Mikahil <toustsikmikhail@gmail.com>
2023-06-17 15:37:47 +0200

    deat: all funcs for fs

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'url';
+import fs from "fs";
+import path from "path";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const srcPath = path.join(__dirname, 'files')
+
 const list = async () => {
-    // Write your code here 
+  if(!fs.existsSync(srcPath)) throw new Error('FS operation failed')
+  fs.readdir(srcPath, (err, files) => {
+    if(err) {
+      console.error(err);
+    }
+    console.log('files', files);
+  })
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import path from "path";
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const srcPath = path.join(__dirname, 'files', 'fileToRead.txt');
+console.log('srcPath: ', srcPath);
+
 const read = async () => {
-    // Write your code here 
+  if (!fs.existsSync(srcPath)) throw new Error('FS operation failed')
+  fs.readFile(srcPath, 'utf8', (err, data) => {
+    if(err) console.error(err);
+    console.log('data: ', data);
+  })
 };
 
 await read();
