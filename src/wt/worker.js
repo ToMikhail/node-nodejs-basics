e38927a409006dd09ea085bfd5131e9d3f0d236e
Toustsik Mikahil <toustsikmikhail@gmail.com>
2023-06-19 14:49:23 +0200

    feat: add thread funcs

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
+import { isMainThread, parentPort, Worker, workerData } from "worker_threads";
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (isMainThread) {
+    throw new Error('This is The main Thread')
+  }
+
+  const res = nthFibonacci(workerData);
+  console.log('res: ', res);
+  parentPort.postMessage(res);
 };
 
 sendResult();
